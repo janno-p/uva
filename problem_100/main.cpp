906b688b33e11c2e416fba0dea231e62f9b6aa81
Janno Põldma <janno.poldma@gmail.com>
2011-08-13 01:20:01 +0300

    Fixed code formatting

--- problem_100/main.cpp
+++ problem_100/main.cpp
@@ -9,31 +9,31 @@ using namespace std;
 
 int cycle_length(int n)
 {
-	int temp = n, length = 1;
-	while (temp != 1)
-	{
-		temp = (temp % 2 == 0) ? (temp / 2) : (3 * temp + 1);
-		length++;
-	}
-	return length;
+  int temp = n, length = 1;
+  while (temp != 1)
+  {
+    temp = (temp % 2 == 0) ? (temp / 2) : (3 * temp + 1);
+    length++;
+  }
+  return length;
 }
 
 int main()
 {
-	int a, b;
-	while (cin >> a >> b)
-	{
-		int first = std::min(a, b);
-		int last = std::max(a, b);
-		
-		int max = 0;
-		for (int i = first; i <= last; i++)
-		{
-			int current = cycle_length(i);
-			max = std::max(current, max);
-		}
-		cout << a << " " << b << " " << max << endl;
-	}
-	
-	return 0;
+  int a, b;
+  while (cin >> a >> b)
+  {
+    int first = std::min(a, b);
+    int last = std::max(a, b);
+    
+    int max = 0;
+    for (int i = first; i <= last; i++)
+    {
+      int current = cycle_length(i);
+      max = std::max(current, max);
+    }
+    cout << a << " " << b << " " << max << endl;
+  }
+  
+  return 0;
 }
