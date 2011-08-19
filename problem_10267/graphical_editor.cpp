41433f0a6e3108f483b0552580ef55d8fe517cd3
Janno Poldma <janno.poldma@gmail.com>
2011-08-20 00:24:02 +0300

    Solution for 'Graphical Editor'

--- problem_10267/graphical_editor.cpp
+++ problem_10267/graphical_editor.cpp
@@ -3,120 +3,222 @@
 #include <cstring>
 #include <iostream>
 #include <limits>
-#include <map>
 #include <string>
 
 using namespace std;
 
-typedef void (*Command)(char** table, int* num_columns, int* num_rows);
-
-void create_new_command(char** table, int* num_columns, int* num_rows)
+class Image
 {
-  delete [] table;
-  cin >> *num_columns >> *num_rows;
-  int size = *num_columns * *num_rows;
-  *table = new char[size];
-  memset(*table, 'O', size);
+public:
+	explicit Image();
+	~Image();
+
+	void Clear();
+	void Reset(int width, int height);
+
+	void DrawVerticalLine(int x, int y1, int y2, char color);
+	void DrawHorizontalLine(int x1, int x2, int y, char color);
+	void FillRectangle(int x1, int y1, int x2, int y2, char color);
+	void FillRegion(int x, int y, char color);
+	void SetPixel(int x, int y, char color);
+
+	void Save(const string& name);
+
+private:
+	void ClearMask();
+	void FillMask(int x, int y);
+
+	int size;
+	int width;
+	char* content;
+	bool* mask;
+};
+
+Image::Image() : size(0), width(0)
+{
+	content = new char[size];
+	mask = new bool[size];
 }
 
-void clear_table_command(char** table, int* num_columns, int* num_rows)
+Image::~Image()
 {
-  int size = *num_columns * *num_rows;
-  memset(*table, 'O', size);
+	delete [] content;
+	delete [] mask;
 }
 
-void color_pixel_command(char** table, int* num_columns, int* num_rows)
+void Image::Clear()
 {
-  int x, y;
-  char color;
-  cin >> x >> y >> color;
-  (*table)[(x - 1) + *num_columns * (y - 1)] = color;
+	memset(content, 'O', size);
 }
 
-void draw_vertical_segment_command(char** table, int* num_columns, int* num_rows)
+void Image::ClearMask()
 {
-  int x, y1, y2;
-  char color;
-  cin >> x >> y1 >> y2 >> color;
-  for (int y = y1; y <= y2; y++)
-    (*table)[(x - 1) + *num_columns * (y - 1)] = color;
+	memset(mask, false, size);
 }
 
-void draw_horizontal_segment_command(char** table, int* num_columns, int* num_rows)
+void Image::Reset(int width, int height)
 {
-  int x1, x2, y;
-  char color;
-  cin >> x1 >> x2 >> y >> color;
-  for (int x = x1; x <= x2; x++)
-    (*table)[(x - 1) + *num_columns * (y - 1)] = color;
+	this->width = width;
+	this->size = width * height;
+
+	delete [] content;
+	content = new char[size];
+
+	delete [] mask;
+	mask = new bool[size];
+
+	Clear();
 }
 
-void draw_filled_rectangle_command(char** table, int* num_columns, int* num_rows)
+void Image::DrawVerticalLine(int x, int y1, int y2, char color)
 {
-  int x1, y1, x2, y2;
-  char color;
-  cin >> x1 >> y1 >> x2 >> y2 >> color;
-  for (int x = x1; x <= x2; x++)
-    for (int y = y1; y <= y2; y++)
-      (*table)[(x - 1) + *num_columns * (y - 1)] = color;
+	int first = min(y1, y2);
+	int last = max(y1, y2);
+	for (int y = first; y <= last; y++)
+		SetPixel(x, y, color);
 }
 
-void fill_region_command(char** table, int* num_columns, int* num_rows)
+void Image::DrawHorizontalLine(int x1, int x2, int y, char color)
 {
-  int x, y;
-  char color;
-  cin >> x >> y >> color;
-  x--; y--;
-
-  // TODO!
+	int first = min(x1, x2);
+	int last = max(x1, x2);
+	for (int x = first; x <= last; x++)
+		SetPixel(x, y, color);
 }
 
-void save_picture_command(char** table, int* num_columns, int* num_rows)
+void Image::FillRectangle(int x1, int y1, int x2, int y2, char color)
 {
-  string name;
-  cin >> name;
-  cout << name << endl;
-  int size = *num_columns * *num_rows;
-  for (int i = 0; i < size; i++)
-  {
-    if (i > 0 && (i % *num_columns) == 0)
-      cout << endl;
-    cout << (*table)[i];
-  }
-  cout << endl;
+	int first_x = min(x1, x2);
+	int last_x = max(x1, x2);
+	int first_y = min(y1, y2);
+	int last_y = max(y1, y2);
+	for (int x = first_x; x <= last_x; x++)
+		for (int y = first_y; y <= last_y; y++)
+			SetPixel(x, y, color);
+}
+
+void Image::FillMask(int x, int y)
+{
+	int index = x + width * y;
+	mask[index] = true;
+
+	if (x > 0 && !mask[index - 1] && content[index - 1] == content[index])
+		FillMask(x - 1, y);
+	if ((x + 1) < width && !mask[index + 1] && content[index + 1] == content[index])
+		FillMask(x + 1, y);
+
+	if (y > 0 && !mask[index - width] && content[index - width] == content[index])
+		FillMask(x, y - 1);
+	if ((index + width) < size && !mask[index + width] && content[index + width] == content[index])
+		FillMask(x, y + 1);
+}
+
+void Image::FillRegion(int x, int y, char color)
+{
+	ClearMask();
+	FillMask(x, y);
+
+	for (int i = 0; i < size; i++)
+		if (mask[i])
+			content[i] = color;
+}
+
+void Image::SetPixel(int x, int y, char color)
+{
+	content[x + width * y] = color;
+}
+
+void Image::Save(const string& name)
+{
+	cout << name << endl;
+	for (int i = 0; i < size; i++)
+	{
+		if (i > 0 && (i % width) == 0)
+			cout << endl;
+		cout << content[i];
+	}
+	cout << endl;
 }
 
 int main()
 {
-  map<char, Command> commands;
-  commands.insert(pair<char, Command>('I', create_new_command));
-  commands.insert(pair<char, Command>('C', clear_table_command));
-  commands.insert(pair<char, Command>('L', color_pixel_command));
-  commands.insert(pair<char, Command>('V', draw_vertical_segment_command));
-  commands.insert(pair<char, Command>('H', draw_horizontal_segment_command));
-  commands.insert(pair<char, Command>('K', draw_filled_rectangle_command));
-  commands.insert(pair<char, Command>('F', fill_region_command));
-  commands.insert(pair<char, Command>('S', save_picture_command));
+	Image image;
 
-  char* table = new char[0];
-  int num_columns = 0, num_rows = 0;
+	char command_char;
+	while (cin >> command_char && command_char != 'X')
+	{
+		switch (command_char)
+		{
+		case 'I':
+			{
+				int width, height;
+				cin >> width >> height;
+				image.Reset(width, height);
+			}
+			break;
 
-  char command;
-  while (cin >> command && command != 'X')
-  {
-    map<char, Command>::iterator it = commands.find(command);
-    if (it == commands.end())
-    {
-      cin.ignore(numeric_limits<streamsize>::max(), '\n');
-      continue;
-    }
+		case 'C':
+			image.Clear();
+			break;
 
-    (it->second)(&table, &num_columns, &num_rows);
-  }
+		case 'L':
+			{
+				int x, y;
+				char color;
+				cin >> x >> y >> color;
+				image.SetPixel(x - 1, y - 1, color);
+			}
+			break;
 
-  delete [] table;
+		case 'V':
+			{
+				int x, y1, y2;
+				char color;
+				cin >> x >> y1 >> y2 >> color;
+				image.DrawVerticalLine(x - 1, y1 - 1, y2 - 1, color);
+			}
+			break;
 
-  return 0;
+		case 'H':
+			{
+				int x1, x2, y;
+				char color;
+				cin >> x1 >> x2 >> y >> color;
+				image.DrawHorizontalLine(x1 - 1, x2 - 1, y - 1, color);
+			}
+			break;
+
+		case 'K':
+			{
+				int x1, x2, y1, y2;
+				char color;
+				cin >> x1 >> y1 >> x2 >> y2 >> color;
+				image.FillRectangle(x1 - 1, y1 - 1, x2 - 1, y2 - 1, color);
+			}
+			break;
+
+		case 'F':
+			{
+				int x, y;
+				char color;
+				cin >> x >> y >> color;
+				image.FillRegion(x - 1, y - 1, color);
+			}
+			break;
+
+		case 'S':
+			{
+				string name;
+				cin >> name;
+				image.Save(name);
+			}
+			break;
+		}
+
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	}
+
+	return 0;
 }
 
 #endif
